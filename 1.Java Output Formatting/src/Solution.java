b5ecc2769411fe89f72baeddf5cbb5d5d7e103c8
ItsThash <thashmikarathnayake11@gmail.com>
2025-08-28 15:00:00 +0530

    Task 3 added

--- 1.Java Output Formatting/src/Solution.java
+++ 1.Java Output Formatting/src/Solution.java
@@ -13,7 +13,7 @@
 
  import java.util.Scanner;
 
- public class JavaOutputFormatting {
+ public class Solution {
 
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
---
+++ 3.Java loops II/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Enter number of iterations : ");
+                int t = in.nextInt();
+
+                for (int i = 0; i < t; i++) {
+                    System.out.print("Enter A : ");
+                    int a = in.nextInt();
+                    System.out.print("Enter b : ");
+                    int b = in.nextInt();
+                    System.out.print("Enter N : ");
+                    int n = in.nextInt();
+
+                    double seq = a + (Math.pow(2, 0) * b);
+                    System.out.print("corresponding series --> " + (int) seq + " ");
+                    for (int j = 1; j < n; j++) {
+                        seq = seq + Math.pow(2, j) * b;
+                        int ans = (int) seq;
+                        System.out.print(ans + " ");
+                    }
+                    System.out.println("\n");
+
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Invalid input and Try again.");
+                in.nextLine();
+
+            }
+        }
+    }
+
+
+}
